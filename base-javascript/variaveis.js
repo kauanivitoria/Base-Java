2210f920215e23e3e5d261b4605150557dc90b70
kauanivitoria <140725971+kauanivitoria@users.noreply.github.com>
2024-08-14 10:40:47 -0300

    Add files via upload

---
+++ base-javascript/variaveis.js
@@ -0,0 +1,45 @@
+// Variáveis
+//tipo Number
+
+const meuNumero = 6;
+const numeroUM = 2;
+const numeroDois = 7;
+
+
+const soma = numeroDois + meuNumero;
+console.log(soma);
+
+const subtracao = numeroDois - meuNumero;
+console.log(subtracao);
+
+const multiplicao = numeroDois * 10;
+console.log(multiplicao);
+
+const divisao = numeroUM / numeroDois;
+console.log(divisao);
+
+//float ( Número decimal )
+
+const numeroTres = 3.3;
+const numeroQuatro = .5 // mesma coias que 0.5
+
+const operacaoM = numeroTres * numeroQuatro
+console.log(operacaoM)
+
+// Nan not a Number 
+
+const operacaoN = "a" * meuNumero
+console.log(operacaoN)
+
+const novaOperacao = "a" + meuNumero
+console.log(novaOperacao);
+// string (texto)
+
+const meuNome = "kauani";
+const frase = 'Meu nome é:';
+const issoNaoEumNumero = '50';
+
+const minhaFrase = Frase + meuNome;
+console.log(minhaFrase);
+
+const operacaoO = issoNaoEumNumero + meuNumero;
